d725d9d3dc27b9f2277cb37cee9fa3ea02a4c1da
Karapus <evgeny.nay@outlook.com>
2020-10-17 11:30:51 +0300

    mycat

--- mycat.c
+++ mycat.c
@@ -1,32 +1,57 @@
-#include <stdlib.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <stdio.h>
+#include <errno.h>
+#include <assert.h>
+#include <limits.h>
+#include <fcntl.h>
 
-#define BUF_CAP 256
-
-int main(int argc, char **argv)
-{
-	for (int i = 1; i < argc; i++)
-	{
-		int fd = open(argv[i], O_RDONLY);
-		void *buf = calloc(BUF_CAP, 1);
-		long res = 0;
-		size_t r_pos = 0;
-		for (; r_pos < BUF_CAP; r_pos += res)
-		{
-			if ((res = read(fd, buf + r_pos, BUF_CAP - r_pos)) == 0)
-				break;
-			if (res < 0)
-				perror("");
+#define BUF_SIZE 128
+ssize_t fdread(int fd, void *buf, size_t count) {
+	assert(fd >= 0);
+	assert(buf);
+	assert(count < SSIZE_MAX);
+	ssize_t read_res;
+	void *cur = buf;
+	while ((read_res = read(fd, cur, count))) {
+		if (read_res < 0) {
+			perror("Read error:");
+			return cur - buf;
 		}
-		res = 0;
-		for (int w_pos = 0; w_pos < BUF_CAP; w_pos += res)
-		{
-			if ((res = write(2, buf + w_pos, BUF_CAP - w_pos)) == 0)
-				break;
-			if (res < 0)
-				perror("");
-		}
+		cur += read_res;
+		count -= read_res;
 	}
+	return cur - buf;
+}
+
+ssize_t fdwrite(int fd, const void *buf, size_t count) {
+	assert(fd >= 0);
+	assert(buf);
+	assert(count < SSIZE_MAX);
+	ssize_t write_res;
+	const void *cur = buf;
+	while ((write_res = write(fd, cur, count))) {
+		if (write_res < 0) {
+			perror("Write error:");
+			return cur - buf;
+		}
+		cur += write_res;
+		count -= write_res;
+	}
+	return cur - buf;
+}
+
+void fdecho(int fd_in, int fd_out, void *buf, size_t size) {
+	ssize_t res;
+	while ((res = fdread(fd_in, buf, size)))
+		fdwrite(fd_out, buf, res);
+}
+
+int main(int argc, char **argv) {
+	int fd = (argc == 2) ? open(argv[1], O_RDONLY) : STDIN_FILENO;
+	if (fd < 0) {
+		perror("Open error:");
+		return errno;
+	}
+	char buf[BUF_SIZE];
+	fdecho(fd, STDOUT_FILENO, buf, BUF_SIZE);
 }
