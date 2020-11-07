c4b07a3c31a39cb8f0e0267059c4e52e7685dcb5
Karapus <evgeny.nay@outlook.com>
2020-11-07 16:49:31 +0300

    count: global vars

--- count.c
+++ count.c
@@ -2,54 +2,43 @@
 #include <pthread.h>
 #include <stdio.h>
 
-
-struct args_t {
-	int n;
-	int *p_num;
-	pthread_mutex_t *m;
-};
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+int number;
 
 void *count1(void *args) {
-	int n = ((struct args_t *) args)->n;
-	int *p_num = ((struct args_t *) args)->p_num;
+	int n = *((int *)args);
 	for (int i = 0; i < n; i++)
-		(*p_num)++;
+		number++;
 	return NULL;
 }
 
 void *count2(void *args) {
-	int n = ((struct args_t *) args)->n;
-	int *p_num = ((struct args_t *) args)->p_num;
-	pthread_mutex_t *m = ((struct args_t *) args)->m;
+	int n = *((int *)args);
 	for (int i = 0; i < n; i++) {
-		pthread_mutex_lock(m);
-		(*p_num)++;
-		pthread_mutex_unlock(m);
+		pthread_mutex_lock(&mutex);
+		number++;
+		pthread_mutex_unlock(&mutex);
 	}
 	return NULL;
 }
 
 void *count3(void *args) {
-	int n = ((struct args_t *) args)->n;
-	int *p_num = ((struct args_t *) args)->p_num;
-	pthread_mutex_t *m = ((struct args_t *) args)->m;
-	pthread_mutex_lock(m);
+	int n = *((int *)args);
+	pthread_mutex_lock(&mutex);
 	for (int i = 0; i < n; i++)
-		(*p_num)++;
-	pthread_mutex_unlock(m);
+		number++;
+	pthread_mutex_unlock(&mutex);
 	return NULL;
 }
 
 void *count4(void *args) {
-	int n = ((struct args_t *) args)->n;
-	int *p_num = ((struct args_t *) args)->p_num;
-	pthread_mutex_t *m = ((struct args_t *) args)->m;
+	int n = *((int *) args);
 	int num;
 	for (int i = 0; i < n; i++)
 		num++;
-	pthread_mutex_lock(m);
-	*p_num += num;
-	pthread_mutex_unlock(m);
+	pthread_mutex_lock(&mutex);
+	number += num;
+	pthread_mutex_unlock(&mutex);
 	return NULL;
 }
 
@@ -61,19 +50,16 @@ int main(int argc, char **argv) {
 	int n = atoi(argv[1]);
 	int m = atoi(argv[2]);
 	int alg = atoi(argv[3]);
-	int number = 0;
-	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-	struct args_t args = {n / m, &number, &mutex};
 	pthread_t ptids[m];
 	pthread_t *ptid = ptids;
+	int n_th = n / m;
 	int n_last = n;
 	fun_t *algs[] = {count1, count2, count3, count4};
 	for (int i = 0; i < m - 1; i++) {
-		pthread_create(ptid++, NULL, algs[alg - 1], &args);
-		n_last -= args.n;
+		pthread_create(ptid++, NULL, algs[alg - 1], &n_th);
+		n_last -= n_th;
 	}
-	struct args_t lastargs = {n_last, &number, &mutex};
-	pthread_create(ptid++, NULL, algs[alg - 1], &lastargs);
+	pthread_create(ptid++, NULL, algs[alg - 1], &n_last);
 	for (pthread_t *p = ptids; p < ptids + m; p++)
 		pthread_join(*p, NULL);
 	printf("%d\n", number);
