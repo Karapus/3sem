ef361d915644ede03754467c7ba3e6f9b9fb55b9
Karapus <evgeny.nay@outlook.com>
2020-12-12 15:33:28 +0300

    unique users

--- unique_users.c
+++ unique_users.c
@@ -6,38 +6,20 @@
 
 #define BUF_SIZE 1024
 
-ssize_t fdread(int fd, void *buf, size_t count) {
+ssize_t fdreadline(int fd, char *buf, size_t count) {
         ssize_t read_res;
-        void *cur = buf;
-        while ((read_res = read(fd, cur, count))) {
+        char *cur = buf;
+        while ((read_res = read(fd, cur, 1))) {
                 if (read_res < 0) {
                         perror("Read error:");
                         return cur - buf;
                 }
+                if (*cur == '\n')
+                	return cur - buf;
                 cur += read_res;
                 count -= read_res;
         }
         return cur - buf;
-}
-
-ssize_t fdwrite(int fd, const void *buf, size_t count) {
-        ssize_t write_res;
-        const void *cur = buf;
-        while ((write_res = write(fd, cur, count))) {
-                if (write_res < 0) {
-                        perror("Write error:");
-                        return cur - buf;
-                }
-                cur += write_res;
-                count -= write_res;
-        }
-        return cur - buf;
-}
-
-void fdecho(int fd_in, int fd_out, void *buf, size_t size) {
-        ssize_t res;
-        while ((res = fdread(fd_in, buf, size)))
-                fdwrite(fd_out, buf, res);
 }
 
 int main(int argc, char **argv) {
@@ -72,16 +54,13 @@ int main(int argc, char **argv) {
 	}
 	wait(-1);
 	char *buf[BUF_SIZE + 1];
+	char *prev[BUF_SIZE + 1];
 	buf[BUF_SIZE] = '\0';
-	while (fdread(fds[0], buf, BUF_SIZE)) {
-		char *first_word = strtok(buf, " ");
-		printf("%s\n", first_word);
-		strtok(NULL, "\n");
-		for (char *second_word = strtok(NULL, " "); strtok(NULL, "\n"); second_word = strtok(NULL, " ")) {
-//			if (!strcmp(first_word, second_word))
-			printf("%s\n", first_word);
-				printf("%s\n", second_word);
-			first_word = second_word;
-		}
+	prev[BUF_SIZE] = '\0';
+	while (fdreadline(fds[0], buf, BUF_SIZE)) {
+		strtok(buf, " ");
+		if (strcmp(buf, prev))
+			printf("%s\n", buf);
+		strcpy(prev, buf);
 	}
 }
